376048a688405997a45f9154f38943a3797e47ef
manan-2702 <mananrajpara2002@gmail.com>
2022-12-09 11:58:01 +0530

    MVC

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 const path = require("path");
+
 const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-const bodyParser = require("body-parser");
-const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/admin", adminRoutes);
+
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { docTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,10 @@
 const path = require("path");
-const rootDir = require("../util/path");
+const productsController = require("../controllers/products");
 const express = require("express");
 
 const router = express.Router();
 
-router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"));
-});
+router.get("/add-product", productsController.getAddProduct);
 
-router.post("/add-product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
+router.post("/add-product", productsController.postAddProducts);
 module.exports = router;
