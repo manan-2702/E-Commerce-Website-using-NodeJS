baa00410af9235990b63175165e8167dfb261b69
manan-2702 <mananrajpara2002@gmail.com>
2023-01-27 00:58:35 +0530

    section12_over

--- controllers/shop.js
+++ controllers/shop.js
@@ -76,54 +76,31 @@ exports.postCartDeleteProduct = (req, res, next) => {
   // });
 };
 
-// exports.postOrders = (req, res, next) => {
-//   let fetchedCart;
-//   req.user
-//     .getCart()
-//     .then((cart) => {
-//       fetchedCart = cart;
-//       return cart.getProducts();
-//     })
-//     .then((products) => {
-//       req.user
-//         .createOrder()
-//         .then((order) => {
-//           return order.addProducts(
-//             products.map((product) => {
-//               product.orderItem = { quantity: product.cartItem.quantity };
-//               return product;
-//             })
-//           );
-//         })
-//         .then((result) => {
-//           return fetchedCart.setProducts(null);
-//         })
-//         .then((result) => {
-//           res.redirect("/orders");
-//         })
-//         .catch((err) => {
-//           console.log(err);
-//         });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+exports.postOrders = (req, res, next) => {
+  req.user
+    .addOrder()
+    .then((result) => {
+      res.redirect("/orders");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-// exports.getOrders = (req, res, next) => {
-//   req.user
-//     .getOrders({ include: ["products"] })
-//     .then((orders) => {
-//       res.render("shop/orders", {
-//         docTitle: "Your Orders",
-//         path: "/orders",
-//         orders: orders,
-//       });
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders()
+    .then((orders) => {
+      res.render("shop/orders", {
+        docTitle: "Your Orders",
+        path: "/orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 // exports.getCheckout = (req, res, next) => {
 //   res.render("shop/checkout", {
 //     docTitle: "Checkout",
